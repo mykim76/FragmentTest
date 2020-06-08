3fc76ab2ff7d48e40d7a10013865db630e14a2d4
mykim76 <iyoungfm@gmail.com>
2020-06-08 19:54:06 +0900

    MyProfileFragment 초안 완성

--- app/src/main/java/com/example/fragmenttest/adapters/MyPagerAdapter.kt
+++ app/src/main/java/com/example/fragmenttest/adapters/MyPagerAdapter.kt
@@ -3,11 +3,13 @@ package com.example.fragmenttest.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.example.fragmenttest.fragments.MyProfileFragment
+import com.example.fragmenttest.fragments.StudentListFragment
 
 class MyPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
 
     override fun getCount(): Int {
-        return 3
+        return 2
     }
 
     override fun getItem(position: Int): Fragment {
@@ -20,9 +22,8 @@ class MyPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
 //        else {return ThirdFragment();}
 
         return when(position){
-            0->FirstFragment()
-            1->SecondFragment()
-            else->ThirdFragment()
+            0->MyProfileFragment()
+            else->StudentListFragment()
         }
     }
 
--- app/src/main/java/com/example/fragmenttest/fragments/StudentListFragment.kt
+++ app/src/main/java/com/example/fragmenttest/fragments/StudentListFragment.kt
@@ -1,4 +1,18 @@
 package com.example.fragmenttest.fragments
 
-class StudentListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmenttest.R
+
+class StudentListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_student_list)
+    }
 }
